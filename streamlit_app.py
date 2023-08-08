484baaf8dde0eb8dd0b483b2c7a7e2b6f9da5e5b
Lynette9620 <141701506+Lynette9620@users.noreply.github.com>
2023-08-08 15:43:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,11 +36,11 @@ streamlit.dataframe(fruityvice_normalized);
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-user = "Lynette2096",
-password = "Lynette@2096",
-account = "https://ep16884.ca-central-1.aws.snowflakecomputing.com",
-warehouse = PC_RIVERY_WH",
-database = "PC_RIVERY_DB",
+user = "Lynette2096"
+password = "Lynette@2096"
+account = "https://ep16884.ca-central-1.aws.snowflakecomputing.com"
+warehouse = PC_RIVERY_WH"
+database = "PC_RIVERY_DB"
 schema = "PUBLIC";
 
 my_cur = my_cnx.cursor()

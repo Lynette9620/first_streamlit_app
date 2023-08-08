bbc82a75f9185283c4a089d877c983a0593d94e8
Lynette9620 <141701506+Lynette9620@users.noreply.github.com>
2023-08-08 15:41:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ warehouse = "pc_rivery_wh",
 database = "pc_rivery_db",
 schema = "public";
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
+my_cur.execute("select * from pc_rivery_db.public.FRUIT_LOAD_LIST")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row);

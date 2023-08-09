5f5236ac375d534fff14583f9e514fa901fc304e
Lynette9620 <141701506+Lynette9620@users.noreply.github.com>
2023-08-09 12:36:17 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,24 +50,23 @@ def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
       my_cur.execute("select * from fruit_load_list")
       return my_cur.fetchall()
+
+ if streamlit.button('Get Fruit List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+     streamlit.dataframe(my_data_rows)
+
+def insert_row_snowflakes(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ("jackfruit", "papaya", "guava" and "kiwi")")
+    return "Thanks for adding " + new_fruit
       
-      if streamlit.button('Get Fruit List'):
-          my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-          my_data_rows = get_fruit_load_list()
-          
-          streamlit.dataframe(my_data_rows)
-          
-          def insert_row_snowflakes(new_fruit):
-              with my_cnx.cursor() as my_cur:
-                  my_cur.execute("insert into fruit_load_list values ('jackfruit' + 'papaya' +  'guava' + 'kiwi')")
-                  return "Thanks for adding " + new_fruit
-                  
-                  add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-                  if streamlit.button('Add a Fruit to the List'):
-                      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-                      streamlit.text(back_from_function)
-                      
-                      streamlit.write('Thanks for adding new_fruit', add_my_fruit)
+      add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  streamlit.text(back_from_function)
+
+streamlit.write('Thanks for adding jackfruit', add_my_fruit)
                       
 
 
